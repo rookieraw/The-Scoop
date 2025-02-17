b67997ea489767361f3956648d2b6586ac087a41
rookieraw <qaz830612@gmail.com>
2025-02-17 15:57:54 +0800

    Add /comments/:id/downvote PUT method

--- server.js
+++ server.js
@@ -38,6 +38,9 @@ const routes = {
   },
   '/comments/:id/upvote': {
     'PUT': upvoteComment
+  },
+  '/comments/:id/downvote': {
+    'PUT': downvoteComment
   }
 };
 
@@ -320,8 +323,24 @@ function upvoteComment(url, request) {
   const username = request.body && request.body.username;
   let savedComment = database.comments[id];
   const response = {};
-  if (id && savedComment && database.users[username]) {
+  if (savedComment && database.users[username]) {
     savedComment = upvote(savedComment, username);
+
+    response.body = { comment: savedComment };
+    response.status = 200;
+  } else {
+    response.status = 400;
+  }
+  return response;
+}
+
+function downvoteComment(url, request) {
+  const id = Number(url.split('/').filter(segment => segment)[1]);
+  const username = request.body && request.body.username;
+  let savedComment = database.comments[id];
+  const response = {};
+  if (savedComment && database.users[username]) {
+    savedComment = downvote(savedComment, username);
 
     response.body = { comment: savedComment };
     response.status = 200;
