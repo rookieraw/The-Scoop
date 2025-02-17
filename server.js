f7dce937980cef35480cec1c7fc1f5d345406e0a
rookieraw <qaz830612@gmail.com>
2025-02-17 14:24:54 +0800

    Add /comments/:id DELETE method

--- server.js
+++ server.js
@@ -33,7 +33,8 @@ const routes = {
     'POST': createComment
   },
   '/comments/:id': {
-    'PUT': updateComment
+    'PUT': updateComment,
+    'DELETE': deleteComment
   }
 };
 
@@ -289,6 +290,24 @@ function updateComment(url, request) {
     savedComment.body = requestComment.body || savedComment.body;
     response.body = { comment: savedComment };
     response.status = 200;
+  }
+  return response;
+}
+
+function deleteComment(url, request) {
+  const id = Number(url.split('/').filter(segment => segment)[1]);
+  const savedComment = database.comments[id];
+  const response = {};
+
+  if (savedComment) {
+    database.comments[id] = null;
+    const articleCommentId = database.articles[savedComment.articleId].commentIds;
+    articleCommentId.splice(articleCommentId.indexOf(id), 1);
+    const userCommentIds = database.users[savedComment.username].commentIds;
+    userCommentIds.splice(userCommentIds.indexOf(id), 1);
+    response.status = 204;
+  } else {
+    response.status = 404;
   }
   return response;
 }
