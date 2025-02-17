a66aa07704319e278e0bc7b03c2db101397106b1
rookieraw <qaz830612@gmail.com>
2025-02-17 15:52:21 +0800

    Add /comments/:id/upvote PUT method

--- server.js
+++ server.js
@@ -35,6 +35,9 @@ const routes = {
   '/comments/:id': {
     'PUT': updateComment,
     'DELETE': deleteComment
+  },
+  '/comments/:id/upvote': {
+    'PUT': upvoteComment
   }
 };
 
@@ -308,6 +311,22 @@ function deleteComment(url, request) {
     response.status = 204;
   } else {
     response.status = 404;
+  }
+  return response;
+}
+
+function upvoteComment(url, request) {
+  const id = Number(url.split('/').filter(segment => segment)[1]);
+  const username = request.body && request.body.username;
+  let savedComment = database.comments[id];
+  const response = {};
+  if (id && savedComment && database.users[username]) {
+    savedComment = upvote(savedComment, username);
+
+    response.body = { comment: savedComment };
+    response.status = 200;
+  } else {
+    response.status = 400;
   }
   return response;
 }
